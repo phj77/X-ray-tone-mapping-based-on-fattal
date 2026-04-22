1d420fccf66dee5ca816640a6145ac4dc3d8c02c
mtr203 <trieutm.hcmute@gmail.com>
2026-04-22 15:18:11 +0900

    optimize code for gradient.py

--- gradient.py
+++ gradient.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# 2026.04.22 - 11:51
+# 2026.04.22 - last updated at 11:51
 def compute_gradient(img):
     """Compute gradient ∇ (using np.gradient)"""
     gx = np.gradient(img, axis=0)
@@ -8,7 +8,7 @@ def compute_gradient(img):
     return gx, gy
 
 
-# 2026.04.22 - 11:51
+# 2026.04.22 - last updated at 11:51
 def compute_divergence(px, py):
     """Compute divergence (using np.gradient for discretization)"""
     dpx_dx = np.gradient(px, axis=0)
