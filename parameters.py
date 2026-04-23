c74a9f8492fb5cdf4c358d2f5bb254a7cf1c6bae
mtr203 <trieutm.hcmute@gmail.com>
2026-04-23 11:47:07 +0900

    update the parameters

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 
 class Parameters:
     def __init__(self):
-        self.gamma_values = [0.51, 0.50, 0.43, 0.45] # in paper: sect. 3.1
+        self.gamma_values = [0.51] # in paper: sect. 3.1
         self.delta = 0.05 # in paper: sect. 3.1
         self.eps = 1e-8
         self.neighbor_size = 3 # the neighborhood pixel number, in paper: setc. 2.2.2.1
